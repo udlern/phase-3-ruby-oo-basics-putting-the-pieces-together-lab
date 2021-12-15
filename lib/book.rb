4b3af738763bd6d7b5c24ea3d3cb8c33b766fd7f
Noa Udler <udlernoa4@gmail.com>
2021-12-15 10:21:44 -0800

    commit tree 6a8758cfe9f96431328216e081ba192e814ffad4

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    attr_reader :turn_page
+    def initialize(title)
+        @title = title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+b1 = Book.new("And Then She Was Gone")
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+require 'pry'
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    attr_reader :cobble
+    def initialize(brand)
+        @brand = brand
+    end
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
+
+s1 = Shoe.new("Gucci")
+binding.pry
